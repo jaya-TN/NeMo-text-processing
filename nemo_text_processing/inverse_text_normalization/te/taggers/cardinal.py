81ec0e6c75974e6e34a992c0713efae44ab3191c
jaya-TN <seruganti@nvidia.com>
2026-08-12 12:35:19 +0530

    feat(te): add Telugu ITN decimal semiotic class
    Signed-off-by: jaya-TN <seruganti@nvidia.com>

--- nemo_text_processing/inverse_text_normalization/te/taggers/cardinal.py
+++ nemo_text_processing/inverse_text_normalization/te/taggers/cardinal.py
@@ -64,8 +64,6 @@ class CardinalFst(GraphFst):
             | ((graph_people | graph_special) @ two_te_digits)
         )
         two_digit_or_zeros = graph_two_digit | pynutil.insert("౦౦")
-
-        optional_case = pynini.closure(graph_case_suffix, 0, 1)
         optional_one = pynini.closure(pynutil.delete("ఒక") + delete_space, 0, 1)
 
         delete_hundred = graph_hundred @ pynini.accep("")
@@ -112,9 +110,10 @@ class CardinalFst(GraphFst):
 
         graph_leading_zeros = graph_zero + pynini.closure(delete_space + (graph_zero | graph_digit), 1)
 
-        graph = (graph_number | graph_zero) + optional_case
+        graph = graph_number | graph_zero
         graph |= graph_leading_zeros
         graph = graph.optimize()
+        self.graph_no_exception = graph
 
         optional_minus_graph = pynini.closure(
             pynutil.insert("negative: ") + pynini.cross(MINUS, "\"-\"") + NEMO_SPACE,
